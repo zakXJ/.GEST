7c29a6fe88b37c6ed81ac93174de5dccfcc119ae
zak bo <xj10.zak@hotmail.com>
2018-05-22 10:50:32 +0100

    test4

--- AppGest/AppGest/Form1.Designer.cs
+++ AppGest/AppGest/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
